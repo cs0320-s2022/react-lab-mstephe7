0e2367809f809293595f27b5563c8b863f63600a
mstephe7 <matthew_stephens@brown.edu>
2022-05-06 12:40:53 -0400

    -m

--- reactlabapp/src/TextBox.tsx
+++ reactlabapp/src/TextBox.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 
 function TextBox(props : any) {
+    const changeHandler = (event : React.FormEvent<HTMLInputElement>) : void => {props.change(event.currentTarget.value)}
     return (
         <div className="TextBox">
+            <input type={'text'} onChange={changeHandler}></input>
             <label>
                 <p>
                     {props.label}
